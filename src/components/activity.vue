763a410af6135e621af39d045997626072f32c14
Design <design@DesigndeMac-mini.local>
2018-03-23 17:46:26 +0800

    响应式

---
+++ src/components/activity.vue
@@ -0,0 +1,124 @@
+<template>
+    <div>
+        <div class="banner">
+            <img src="../assets/images/banner.jpg" alt="">
+        </div>
+        <div class="main">
+            <div class="main-left">
+                <img src="../assets/images/lantern.png" alt="">
+            </div>
+            <div class="titles">
+                <ul>
+                    <li v-for="title,index in titles" :class="{ active: isActive==index }" @click="changActive(index)">{{title}}</li>
+                </ul>
+                <div class="content">
+                    <div :is="isItem"></div>
+                </div>
+            </div>
+            <div class="main-right">
+                <img src="../assets/images/lantern.png" alt="">
+            </div>
+            
+        </div>
+        
+    </div>
+</template>
+
+<script>
+    import item0 from "./activity_item0"
+    import item1 from "./activity_item1"
+    import item2 from "./activity_item2"
+    export default {
+        components: {
+            item0,
+            item1,
+            item2
+        },
+        data() {
+            return {
+                titles:["流水排行","活动介绍","活动奖励"],
+                isActive: "0",
+                isItem: "item0"
+            }
+        },
+        methods: {
+            changActive (index) {
+                this.isActive = index
+                this.isItem = "item" + index
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .banner img{
+        width:100%;
+        vertical-align:middle;
+    }
+    .main{
+        display: flex;
+        justify-content:space-between;
+        background: url("../assets/images/spring_bg.png")
+    }
+    .titles{
+        width: 820px;
+    }
+    .titles ul{
+        display: flex;
+        justify-content:space-between;
+        width: 100%;
+        height: 38px;
+        margin-bottom: 20px;
+    }
+    .main-left{
+        padding-left: 100px;
+    }
+    .main-right{
+        padding-right: 100px;
+    }
+    .titles ul li{
+        color: #860406;
+        background-image: url("../assets/images/arrow_left_red.png"),url("../assets/images/arrow_right_red.png");
+        background-repeat: no-repeat;
+        background-position: left,right;
+        line-height: 26px;
+        padding: 6px 16px;
+        width: 100px;
+        cursor: pointer;
+        text-align: center;
+    }
+    .titles ul li.active{
+        background-image: url("../assets/images/arrow_left_yellow.png"),url("../assets/images/arrow_right_yellow.png");
+        color:#FEF8A0;
+    }
+    .titles ul li:hover{
+        color:#FEF8A0;
+    }
+    .content {
+        background-image: url("../assets/images/corner_yellow_lt.png"),url("../assets/images/corner_yellow_rt.png"),url("../assets/images/corner_yellow_lb.png"),url("../assets/images/corner_yellow_rb.png");
+        background-repeat: no-repeat;
+        background-position: left top,right top,left bottom,right bottom;
+        padding:20px 29px 23px 30px;
+    }
+    @media screen and (max-width: 1000px){
+        .main-left{
+            display: none;
+        }
+        .main-right{
+            display: none;
+        }
+        .content{
+            box-sizing: border-box;
+            width: 100%;
+        }
+        .titles{
+            width: 100%;
+        }
+    }
+    @media screen and (max-width: 627px){
+        .content{
+            padding: 0;
+            background: none;
+        }
+    }
+</style>
---
+++ src/components/activity_item2.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="box">
+        <p>本次活动分为现金奖励和积分奖励，其中：</p>
+        <ul>
+            <li class="money">
+                <div>现金奖励</div>
+                <p>
+                    第一名: <span>8888现金；</span><br>
+                    第一名: <span>6666现金；</span><br>
+                    第一名: <span>55555现金；</span>
+                </p>
+            </li>
+            <li class="coin">
+                <div>积分奖励：</div>
+                <p>从第4名开始计算，4-88名的认购排名客户均获赠积分奖励。我们将准备出<span>5888</span>积分构成奖励积分池，入围的85位VIP将按照自己认购的金额获赠相应额度的积分，积分可以直接兑换尊耀汇网站商城的礼品，也可以保留到下次使用。</p>
+            </li>
+            <p>积分奖励算法：个人奖励积分=奖励回馈总额/入围客户认购的流水总额×个人认购金额。</p>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        },
+    }
+</script>
+
+<style scoped>
+     .box{
+        padding:20px;
+        background-color:rgba(127,10,13,0.6);
+        font-size: 16px;
+        line-height: 36px;
+        font-family: "Segoe UI","Lucida Grande",Helvetica,Arial,"Microsoft YaHei",FreeSans,Arimo,"Droid Sans","wenquanyi micro hei","Hiragino Sans GB","Hiragino Sans GB W3",FontAwesome,sans-serif;
+        color: #ffb4b5;
+        font-weight: 400;
+    }
+    ul li{
+        margin-bottom: 30px;
+    }
+    ul li div{
+        padding-left: 60px;
+    }
+    ul .money div{
+        background:  url("../assets/images/cash.png") no-repeat top left;
+    }
+    ul .coin div{
+        background:  url("../assets/images/coin.png") no-repeat top left;
+    }
+</style>
